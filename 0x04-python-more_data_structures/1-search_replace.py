5d49406032abc6a4a336345580b6f3a8c0a70fbf
Ifeanyit103 <ifeanyit103@gmail.com>
2023-01-11 07:35:46 -0800

    replaces all occurrences of an element

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 
 def search_replace(my_list, search, replace):
     def s_r_elm(elm):
-        return (elm if elm != search, replace)
+        return (elm if elm != search else replace)
     return list(map(s_r_elm, my_list))
